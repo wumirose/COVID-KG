56478d4fc9d7441d4f80db5a8b89da44453f335f
Olawumi Olasunkanmi <30610081+wumirose@users.noreply.github.com>
2023-06-15 10:14:33 -0400

    Update pickmax.py

--- pickmax.py
+++ pickmax.py
@@ -1,17 +1,3 @@
-from copy import deepcopy
-def pick_max_answer(f):
-    f_copy = deepcopy(f)
-    fnal = []
-    for i, new in enumerate(f):
-        item = f_copy[i]
-        new_id = new['node_bindings']['input_chemical'][0]['id']
-        if new_id in [binding['id'] for binding in new['node_bindings']['input_chemical']]:
-            f.remove(item)
-            item.update(new)
-            fnal.append(item)
-    return fnal
-
-
 
 f = [
 
@@ -75,5 +61,20 @@ f = [
 '_n_ac_3_0': [{'id': 'NCBIGene:4050'}]}}]}
 ]
 
+from copy import deepcopy
+def pick_max_answer(f):
+    f_copy = deepcopy(f)
+    fnal = []
+    for i, new in enumerate(f):
+        item = f_copy[i]
+        new_id = new['node_bindings']['input_chemical'][0]['id']
+        if new_id in [binding['id'] for binding in new['node_bindings']['input_chemical']]:
+            f.remove(item)
+            item.update(new)
+            fnal.append(item)
+    return fnal
+
+
+pick_max_answer(f)
 
 
